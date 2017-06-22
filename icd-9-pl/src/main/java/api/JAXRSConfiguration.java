48849e8f90335f20ddbd926add8197d4e9cd9fc2
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-06-22 23:15:03 +0200

    new module for icd-9-pl classification api client

---
+++ icd-9-pl/src/main/java/api/JAXRSConfiguration.java
@@ -0,0 +1,12 @@
+package api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Mariusz Szymanski
+ */
+@ApplicationPath("icd")
+public class JAXRSConfiguration extends Application {
+
+}
