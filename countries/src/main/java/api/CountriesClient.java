78929e9338060077f4be6eb45be521c84f6f8ec2
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-06-22 21:49:01 +0200

    Get country details by full name

--- countries/src/main/java/api/CountriesClient.java
+++ countries/src/main/java/api/CountriesClient.java
@@ -16,15 +16,27 @@ import java.util.List;
 class CountriesClient {
 
     private static final String COUNTRIES_SERVICE_URL
-            = "https://restcountries.eu/rest/v2/all?fields=name;capital;region;population;flag";
+            = "https://restcountries.eu/rest/v2/{code}fields=name;capital;region;population;flag";
 
     List<Country> getAllCountries(){
         final Client client = ClientBuilder.newClient();
-        final WebTarget target = client.target(COUNTRIES_SERVICE_URL);
+        final WebTarget target = client.target(COUNTRIES_SERVICE_URL.replace("{code}", "all?"));
         final Response response = target.request().get();
         final Country[] countries = response.readEntity(Country[].class);
         response.close();
 
         return Arrays.asList(countries);
     }
+
+    Country getCountryDetails(String countryFullName){
+        final Client client = ClientBuilder.newClient();
+        final WebTarget target = client.target(COUNTRIES_SERVICE_URL
+                .replace("{code}", "name/" + countryFullName + "?fullText=true&"));
+        final Response response = target.request().get();
+        final Country[] countries = response.readEntity(Country[].class);
+        response.close();
+
+        return countries[0];
+    }
+
 }
--- countries/src/test/api/CountriesClientTest.java
+++ countries/src/test/api/CountriesClientTest.java
@@ -4,9 +4,13 @@ import domain.Country;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.ProcessingException;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
+
 
 /**
  * @author Mariusz Szymanski
@@ -26,5 +30,36 @@ public class CountriesClientTest {
         final List<Country> countries = countriesClient.getAllCountries();
 //        Then
         assertNotNull(countries);
+    }
+
+    @Test
+    public void shouldContainCountryWithProperValues() throws Exception {
+//        Given
+        String expectedName = "Afghanistan";
+        String expectedCapital = "Kabul";
+//        When
+        final List<Country> countries = countriesClient.getAllCountries();
+        final Country country = countries.get(0);
+//        Then
+        assertThat(country.getName(), is(expectedName));
+        assertThat(country.getCapital(), is(expectedCapital));
+    }
+
+    @Test
+    public void shouldGetCountryDetails() throws Exception {
+//        Given
+        String expectedName = "Poland";
+        String expectedCapital = "Warsaw";
+//        When
+        final Country country = countriesClient.getCountryDetails("poland");
+//        Then
+        assertThat(country.getName(), is(expectedName));
+        assertThat(country.getCapital(), is(expectedCapital));
+    }
+
+    @Test (expected = ProcessingException.class)
+    public void shouldThrowExceptionWhenCountryNotFound() throws Exception {
+//        When
+        countriesClient.getCountryDetails("poolland");
     }
 }
